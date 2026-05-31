05f7b4308de96a9a854349a01ef98b09e0d0b088
Aditya Kumar Yadav <147330521+iAditya001@users.noreply.github.com>
2026-06-01 02:42:20 +0530

    Create BestTBS.java

---
+++ Week_1/DAY_1/BestTBS.java
@@ -0,0 +1,13 @@
+package Week_1.DAY_1;
+
+public class BestTBS {
+    public int maxProfit(int[] p) {
+        int i=p[0];
+        int res=0;
+        for(int k=1;k<p.length;k++){
+             i=Math.min(i,p[k]);
+             res=Math.max(res,p[k]-i);       
+        }
+        return res;          
+    }
+}
