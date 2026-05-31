025d8558ce7f55475adf5bce89be495589102b66
Aditya Kumar Yadav <adityajr.asr07@gmail.com>
2026-06-01 02:36:08 +0530

    "1"

---
+++ Week_1/DAY_1/RemovedS.java
@@ -0,0 +1,21 @@
+package Week_1.DAY_1;
+
+class RemovedS {
+    public int removeDuplicates(int[] nums) {
+        int c=1;
+        int i=0;
+        int j=0;
+        while(j<nums.length){
+           if(nums[i]==nums[j]){
+            j++;
+           }
+           else if(nums[i]!=nums[j]){
+              i++;
+              nums[i]=nums[j];
+              j++;
+              c++;
+           }                
+        }
+        return c;
+    }
+}
